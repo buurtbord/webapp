2e517f4aafb0420c6e3e733665f102f1a4b5346b
Ozaynaci <hsnzync_@hotmail.com>
2017-03-16 15:44:01 +0100

    laravel

---
+++ app/views/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Laravel PHP Framework</title>
+	<link rel="stylesheet" href="/webapp/dist/css/style.css">
+</head>
+<body>
+	<div class="main">
+        <div class="background">
+        
+        
+        </div>
+
+		<h1>Hello World.</h1>
+	</div>
+</body>
+</html>
